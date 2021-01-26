6c5df0a84d55287d6ea66f96b7492dfa5576f631
Roger Bloom <rogerbloom1@gmail.com>
2021-01-26 12:09:42 -0800

    completed exercise

--- app.js
+++ app.js
@@ -5,15 +5,49 @@ console.log(
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = b - a;
+let multiply = a * b;
+let dividing = a / b;
+console.log(add)
+console.log(minus)
+console.log(multiply)
+console.log(dividing)
 
 // YOUR CODE HERE
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
-
+let a = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+console.log(a + str)
+console.log(a + str2)
+console.log(a + isPresent)
+console.log(a + firstName)
+console.log(a = lastName)
 // YOUR CODE HERE
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
-
+let a = 5;
+let str = "5";
+let str2 = "five";
+let isPresent = false;
+let firstName = "Robin";
+let lastName = "Williams";
+console.log(a == str)
+console.log(a === str)
+console.log(!isPresent)
+console.log("eleven" == str2 && a >= str)
+console.log(!isPresent || isPresent)
+console.log(0 == false)
+console.log(0 === false)
+console.log(0 != false)
+console.log(0 !== false)
 // YOUR CODE HERE
